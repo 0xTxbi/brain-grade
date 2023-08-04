61cad0a4da07707c7fd7fd7abd0767163faf092a
0xTxbi <techiejossy@gmail.com>
2023-08-04 06:50:47 +0100

    rwd: init

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -13,24 +13,8 @@ export const metadata: Metadata = {
 export default function AuthPage() {
 	return (
 		<>
-			<div className="md:hidden h-screen bg-black">
-				<Image
-					src="/examples/authentication-light.png"
-					width={1280}
-					height={843}
-					alt="Authentication"
-					className="block dark:hidden"
-				/>
-				<Image
-					src="/examples/authentication-dark.png"
-					width={1280}
-					height={843}
-					alt="Authentication"
-					className="hidden dark:block"
-				/>
-			</div>
-			<div className="container h-screen relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+			<div className="container h-screen relative flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+				<div className="relative h-full hidden flex-col bg-muted p-10 text-white dark:border-r lg:flex">
 					<div className="absolute inset-0 bg-zinc-900" />
 					<div className="relative z-20 flex items-center text-lg font-medium">
 						<svg
@@ -82,12 +66,12 @@ export default function AuthPage() {
 					</div>
 				</div>
 				<div className="lg:p-8">
-					<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+					<div className="mx-auto text-sm flex w-full flex-col justify-center space-y-6">
 						<Tabs
 							defaultValue="sign-up"
 							className="w-[400px]"
 						>
-							<TabsList className="grid w-full grid-cols-2">
+							<TabsList className="grid w-full grid-cols-2 text-sm">
 								<TabsTrigger value="sign-up">
 									Sign Up
 								</TabsTrigger>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/typography/heading";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<main className="flex h-screen flex-col items-center justify-between p-24">
-			<a href="/register">
+			<Heading level={2}>BrainGrade</Heading>
+
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="100"
+					height="100"
+					fill="none"
+					stroke="currentColor"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="2"
+					className="lucide lucide-graduation-cap"
+					viewBox="0 0 24 24"
+				>
+					<path d="M22 10v6M2 10l10-5 10 5-10 5z"></path>
+					<path d="M6 12v5c3 3 9 3 12 0v-5"></path>
+				</svg>
+			</div>
+
+			<p className="md:text-md text-sm text-center text-muted-foreground">
+				Embrace the convenience of instant GPA
+				computations, visualize erudition trends, and
+				set visionary goals to augment your educational
+				prowess.
+			</p>
+
+			<a href="/auth">
 				<Button>Get Started</Button>
 			</a>
 		</main>
